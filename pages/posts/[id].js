d711ae7f2f9d4bd58ce2e1ad795f0e9cbc9cce76
Hebert Montero Garcia <hmontero89@gmail.com>
2020-07-25 15:33:44 +0200

    Fifth lesson. Dynamic Routes. Adds logic to render Markdown with a the library 'remark'

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,8 @@ export default function Post({ postData }) {
             {postData.id}
             <br/>
             {postData.date}
+
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>  
     );
 }
@@ -23,11 +25,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     
     return {
         props: {
             postData
-        }, 
-    }
+        }
+    };
 }
